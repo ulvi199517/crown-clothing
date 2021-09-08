99217fbf628a965c1365dbcdb1b26ec34ae56553
ulvi_199517 <ulvi199517@gmail.com>
2021-09-07 21:51:37 -0400

    created sign-in-and-sign-up page. Also created sign-in component and Form-input component and styled it

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import './sign-in.styles.scss';
+import FormInput from "../form-input/form-input.component";
+class SignIn extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.handleSubmit = event => {
+            event.preventDefault();
+            this.setState({email: '', password: ''});
+        }
+        this.handleChange = (event) => {
+            const {value, name} = event.target;
+            this.setState({[name]: value})
+        }
+    }
+    render(){
+        return(
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        name='email' 
+                        type='email'
+                        label='email'
+                        value={this.state.email} 
+                        handleChange={this.handleChange}
+                        required />
+                    <FormInput 
+                        name='password' 
+                        type='password'
+                        label='password' 
+                        value={this.state.password} 
+                        handleChange={this.handleChange}
+                        required />
+                    <input type='submit' value='Submit Form' />
+                </form>
+            </div>
+        )
+    }
+}
+export default SignIn;
