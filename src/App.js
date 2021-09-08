7f84554022728f9bf243557104af465d32b6ae9f
ulvi_199517 <ulvi199517@gmail.com>
2021-09-08 14:33:31 -0400

    creating userProfileDocument in firebase database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {Route, Switch } from 'react-router-dom';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -19,9 +19,8 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount(){
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-        this.setState({currentUser: user});
-        console.log(user);
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+        createUserProfileDocument(user);
       })
     }
     componentWillUnmount(){
