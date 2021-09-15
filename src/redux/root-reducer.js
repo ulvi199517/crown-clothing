996dbea4dcdcd8c6abd0531da2f12037ad914d83
ulvi_199517 <ulvi199517@gmail.com>
2021-09-15 12:24:24 -0400

    moved directory state of sections into redux store

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from './cart/cart.reducer.js';
+import directoryReducer from "./directory/directory.reducer";
 import {persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
@@ -12,7 +13,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 })
 
 export default persistReducer(persistConfig, rootReducer);
