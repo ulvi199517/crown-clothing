7e3e9ab7fa3c1f5fa39ab8808bb9ae9cdb49c969
ulvi_199517 <ulvi199517@gmail.com>
2021-09-15 16:23:06 -0400

    changed our shop data from array to object, created new shop selector for collections-overview to handle maping over object instead of array

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './collections-overview.styles.scss';
 import {connect} from 'react-redux';
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
 const CollectionsOverview = ({collections}) => (
@@ -15,7 +15,7 @@ const CollectionsOverview = ({collections}) => (
     </div>
 );
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 export default connect(mapStateToProps)(CollectionsOverview);
 
