8ccd063c8a4168d0526f6483bf0c90ca398cf547
ulvi_199517 <ulvi199517@gmail.com>
2021-09-22 19:27:55 -0400

    building backend again

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51Ja5oICGipMf0dDjcPBNyNeGyfkuY8cxyrve2cu00WLqBtktaAFpB8WIXGTL00U4hS4n4DdCB6SSIupeLuM7azHZ00lXPyvPgx';
     
     const onToken = token => {
-        console.log(token);
-        alert('Payment was Successful');
-    }
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
     return(
         <StripeCheckout
             label='Pay Now'
